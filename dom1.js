a5bd85b7d23aafcd08e545002fdb9d5a1d1b15f9
Bhavesh jain <151775274+Darkfalcon2804@users.noreply.github.com>
2024-08-04 10:08:38 +0530

    lecture 6

---
+++ dom1.js
@@ -0,0 +1,4 @@
+console.log("hello");
+window.console.log("hello I am darkfalcon");
+let firstele=document.querySelector("#myId");
+console.dir(firstele);
--- practiceQ.js
+++ practiceQ.js
@@ -1,13 +1,2 @@
-
- ////Q1.
-// for(let i=1;i<=100;i++){
-//     if(i%2==0) console.log("num=",i);
-// }
-    ////Q2.
-    let gamenum=45;
-    let userno = prompt("Guess the game number: ");
-
-    while(userno!=gamenum){
-        userno=prompt("You entered wrong number.Guess again: ");
-    }
-    console.log("congratulations,you entered the right number");
+let h2=document.querySelector("h2");
+console.dir(h2.innerText);
